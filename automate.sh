dd4e6b2d33f343d9405a7146eb9915efbb8c77a3
Daniel Azulay <69147968+TheSerialiZator@users.noreply.github.com>
2021-01-12 17:22:38 +0200

    Create automate.sh

---
+++ automate.sh
@@ -0,0 +1,12 @@
+curl --proxy http://127.0.0.1:8080 -o cacert.der http://burp/cert  \
+&& openssl x509 -inform DER -in cacert.der -out cacert.pem \
+&& cp cacert.der $(openssl x509 -inform PEM -subject_hash_old -in cacert.pem |head -1).0 \
+&& adb root \
+&& adb remount \
+&& adb push $(openssl x509 -inform PEM -subject_hash_old -in cacert.pem |head -1).0 /sdcard/ \
+&& echo -n "mv /sdcard/$(openssl x509 -inform PEM -subject_hash_old -in cacert.pem |head -1).0 /system/etc/security/cacerts/" | adb shell \
+&& echo -n "chmod 644 /system/etc/security/cacerts/$(openssl x509 -inform PEM -subject_hash_old -in cacert.pem |head -1).0" | adb shell \
+&& echo -n "reboot" | adb shell \
+&& rm $(openssl x509 -inform PEM -subject_hash_old -in cacert.pem |head -1).0 \
+&& rm cacert.pem \
+&& rm cacert.der
